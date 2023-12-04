e4a988119951c3263d2ef9b978feace3f7691512
108 <108@academicos.cesde.edu.co>
2023-12-04 15:35:09 -0500

    Se crea proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,37 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+//Entradas del problema
+        String NombreUsuario="Daniel Arango Sanchez";
+        short EdadUsuario=27;
+        String TelefonoUsuario="3144025258";
+        String DocumentoIdentidad="1040742626";
+        int CuotaEnero;
+        int CuotaFebrero;
+        int CuotaMarzo;
+        int CuotaAbril;
+        int CuotaMayo;
+        int CuotaJunio;
+        int CuotaJulio;
+        int CuotaAgosto;
+        int CuotaSeptiembre;
+        int CuotaOctubre;
+        int CuotaNoviembre;
+        String DireccionUsuario="Cra 62 #74-167";
+        double CostoInscripcion=150000.0;
+        float TasaInteresNatillera=0.1F;
+        double  SaldoFinalUsuario;
+
+        //Proceso
+
+        Scanner leerDato =new Scanner(System.in);
+
+        System.out.println("Hola soy una IA, cual es tu nombre?");
+        leerDato.nextLine();
+    }
+
+
+    }
